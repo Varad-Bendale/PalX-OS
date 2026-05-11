c19baf8daad8416f081409095f7a93f77a72e649
Varad Bendale <varadbendale17@gmail.com>
2026-05-11 10:29:47 +0530

    Update gdt.asm

--- Bootloader/gdt.asm
+++ Bootloader/gdt.asm
@@ -33,7 +33,13 @@ dd gdt_start
 ; db -> 23 -> 31 same make it to zero 
 ; here now the last thing is taken because of incovinece intel already did it earlier and then they just added it further nothing can be done 
 ; and just for info dd means the whole 32 bits register for the cpu we did define it in the like gdt null okay so yeah that is taken in consideration as well 
-; now there is this 0xffff these are like see limit to all the bits for the cpu as the main work of gdt is to make the cpu ready for the 32 bit thing work now we have 2 different differetn segments data and ;code so this is the limit for the thing so now ofc this is very low so there is like 1111 hidden thing which is added to the limit which makes the thing like 0xFFFFF which is insanely huge number like a mil;lion bit which is like 1 mb but still its too much low so we got the granuality which makes us use the 4kb inspite of 1 byte so our storage / limit increases significantly makes it to like 4gb which is awes;ome amount of limit the real nesecessity for the limit is like the memory thing see if any of the code / data looks for the memory and picks something if it is anything it would just start giving up random ; stupid ass garbage value so we need to like set up the limit for the thing which makes it better like see there are two segments data and code the process should reach in each others teretoy simply we need; to look for the same thing 
+; now there is this 0xffff these are like see limit to all the bits for the cpu as the main work of gdt is to make the cpu ready for the 32 bit thing work now
+; we have 2 different differetn segments data and code so this is the limit for the thing so now ofc this is very low so there is like 1111 hidden thing which 
+; is added to the limit which makes the thing like 0xFFFFF which is insanely huge number like a million bit which is like 1 mb but still its too much low so we
+; got the granuality which makes us use the 4kb inspite of 1 byte so our storage / limit increases significantly makes it to like 4gb which is awe
+ome amount of limit the real nesecessity for the limit is like the memory thing see if any of the code / data looks for the memory and picks something if it is anything it would just start giving up random 
+; stupid ass garbage value so we need to like set up the limit for the thing which makes it better like see there are two segments data and code the process should reach in each others teretoy simply we need
+; to look for the same thing 
 
 ;for code Access byte 10011010b:
 ;1 → present (segment exists)
