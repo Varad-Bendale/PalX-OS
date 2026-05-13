6f638e06099f0553f64c92e6bfd8680ee3ec772c
Varad Bendale <varadbendale17@gmail.com>
2026-05-13 21:46:44 +0530

    Update Psi.h

--- Kernal/PIC/Psi.h
+++ Kernal/PIC/Psi.h
@@ -8,7 +8,10 @@ ifndef pic
 
 #endif 
 
-/* okay pic are all the interrupts which the cpu use for like the executing all the programmes see the cpu needs a interrupts that okay this function or this programme needs to be executed stop all and focus on this one so thats why the interrupts are needed and also see we are working on the x-86 processor to make the os atp so making it is the thing is or the architecture goes like 
+/* okay pic are all the interrupts which the cpu use for like the executing all the programmes see the cpu needs a interrupts
+that okay this function or this programme needs to be executed stop all and focus on this one so thats why the interrupts are
+needed and also see we are working on the x-86 processor to make the os atp so making it is the thing is or the architecture 
+goes like 
 
 Master PIC          Slave PIC
 ──────────          ──────────
@@ -21,7 +24,12 @@ IRQ 5 → Sound       IRQ 13 → FPU
 IRQ 6 → Floppy      IRQ 14 → Primary Disk
 IRQ 7 → Printer     IRQ 15 → Secondary Disk
 
-At the earlier times there existed only the 8 interrupts port on the cpu so yes they made it like a nested thing to get access to all of them 
-Now listen here all the first 2 are the commands which needs to be given and the last 2 are the data which the interrupts need to carry are like the instructions which are there like follow this and ignore these like that stuff 
+At the earlier times there existed only the 8 interrupts port on the cpu so yes they made it like a nested thing to get access
+to all of them Now listen here all the first 2 are the commands which needs to be given and the last 2 are the data which the
+interrupts need to carry are like the instructions which are there like follow this and ignore these like that stuff 
 
-Now listen here we are like posting all the functions name here reason see the c compiler issue it is see the compiler gives error if it doesn't see the stuff earlier on like in main function t sees the function name and if the function is defined in the lower of the main function then it gives out error now we can be careful with these at one file but at the multiple files it can be the issue to make them in order so we initialise it in the .h files only it is better like it would show error as it would have seen the name earlier one */ 
+Now listen here we are like posting all the functions name here reason see the c compiler issue it is see the compiler gives 
+error if it doesn't see the stuff earlier on like in main function t sees the function name and if the function is defined in 
+the lower of the main function then it gives out error now we can be careful with these at one file but at the multiple files 
+it can be the issue to make them in order so we initialise it in the .h files only it is better like it would show error as it
+would have seen the name earlier one */ 
